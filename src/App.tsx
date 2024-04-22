38803f107b516fb452e5b78dc21a202012246106
Eduardo de Paula <eduardoibarr56@gmail.com>
2024-04-22 14:45:13 -0300

    feat: add 404 page and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/Home";
 import LoginPage from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "login",
+    element: <LoginPage />,
+  },
+  {
+    path: "*",
+    element: <PageNotFound />,
+  },
+]);
 
 export default function App() {
-  return (
-    <div>
-      <LoginPage />
-      <HomePage />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
---
+++ src/pages/PageNotFound/index.tsx
@@ -0,0 +1,25 @@
+function PageNotFound() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-200">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-gray-800 mb-4">
+          404 - Página não encontrada
+        </h1>
+        <p className="text-lg text-gray-600">
+          A página que você está procurando não foi encontrada.
+        </p>
+        <p className="text-lg text-gray-600">
+          Por favor, verifique o URL digitado e tente novamente.
+        </p>
+        <button
+          onClick={() => window.history.back()}
+          className="mt-8 px-6 py-3 bg-gray-800 text-white rounded hover:bg-gray-900 focus:outline-none focus:ring focus:ring-blue-300"
+        >
+          Voltar
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default PageNotFound;
