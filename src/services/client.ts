ce6914336c782871521e015f50f5f03d2467c876
Eduardo de Paula <eduardoibarr56@gmail.com>
2024-04-29 11:48:19 -0300

    feat: add remote api

--- src/services/client.ts
+++ src/services/client.ts
@@ -1,19 +1,16 @@
 import axios from "axios";
-import { getItem } from "../utils/local-storage";
 
 export const client = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "http://34.234.67.233:3333/",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 client.interceptors.request.use((config) => {
-  const token = getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+  // if (token) {
+  //   config.headers.Authorization = `Bearer ${token}`;
+  // }
 
   return config;
 });
